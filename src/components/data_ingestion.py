5e2bf2d543134db2d3a40dc5fa97b8cffe62b180
Jose Mendoza <pball626@gmail.com>
2024-03-09 20:24:55 -0600

    pickle file made, im a picklee

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split # ML libary
 from dataclasses import dataclass
+from data_transformation import DataTransformation
 
 # inputs are required where you will recieve raw data or train data
 
@@ -62,6 +63,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-print('reee')
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
